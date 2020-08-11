f74ffafea8c989ba23904f5a2f3733249a7ae6d8
VictorLM <victordinami@gmail.com>
2020-08-11 15:09:43 -0300

    Correção para não tentar adicionar o próprio ID

--- addContacts.py
+++ addContacts.py
@@ -3,9 +3,12 @@ import json
 
 user = input("User: ") 
 
-sk = Skype(tokenFile=".tokens-" + user)
+sk = Skype(tokenFile=".tokens-" + user) # Carrega o token
+
+currentUserId = sk.user.id # Pega ID do usuários atual
 
 with open("contacts.json") as json_file:
     data = json.load(json_file)
     for value in data:
-        sk.contacts[value["id"]].invite("Poderia me adicionar, por favor?")
+        if value["id"] != currentUserId:
+            sk.contacts[value["id"]].invite("Poderia me adicionar, por favor?")
