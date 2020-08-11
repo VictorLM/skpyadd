07e1d7d3c4ef2aa47718a7206a3933efe1756b96
VictorLM <victordinami@gmail.com>
2020-08-11 15:44:51 -0300

    Correção sleep para não add muito rápido e levantar o Auth rate limit exceeded

--- addContacts.py
+++ addContacts.py
@@ -1,7 +1,10 @@
 from skpy import Skype
 import json
+import time
 
 user = input("User: ") 
+
+message = input("Mensagem a ser enviada no convite: ") 
 
 sk = Skype(tokenFile=".tokens-" + user) # Carrega o token
 
@@ -9,6 +12,9 @@ currentUserId = sk.user.id # Pega ID do usuários atual
 
 with open("contacts.json") as json_file:
     data = json.load(json_file)
+    print ("Cada contato leva 5 segundos para ser adicionado, para evitar bloqueios e exceções.")
     for value in data:
         if value["id"] != currentUserId:
-            sk.contacts[value["id"]].invite("Poderia me adicionar, por favor?")
+            sk.contacts[value["id"]].invite(message)
+            print ("Adicionado o contato: " + value["nome"]) # Se seu contacts.json não tiver as chaves "nome", comente essa linha
+            time.sleep(5) # Para não levantar uma SkypeAuthException "Auth rate limit exceeded"
